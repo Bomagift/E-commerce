b54483a1421c07d627aa446f9e1a00b636fefcf4
boma <94929211+Bomagift@users.noreply.github.com>
2024-06-03 14:46:29 -0400

    fixing images

--- src/E-commerce_landingpage_components/CallToAction.jsx
+++ src/E-commerce_landingpage_components/CallToAction.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import "./CallToAction.css";
+import Background from '../../public/Images/background.png'
 
 function CallToAction() {
   return (
     <div className="Fourth-container">
       <div className="container-large">
-        <img src="/public/Images/background.png" alt="Item" />
+        <img src={Background} alt="Item" />
         <div className="overlay-1">
           <h6>Designing Better Experience</h6>
           <h2>Problems trying to resolve the conflict between </h2>
@@ -15,8 +15,8 @@ function CallToAction() {
           </p>
           <h3>$16.48</h3>
           <div className="button">
-          <button>ADD YOUR CALL TO ACTION</button>
-        </div>
+            <button>ADD YOUR CALL TO ACTION</button>
+          </div>
         </div>
       </div>
     </div>
